50b7971902dae53b5b1a6763e8defae2b8f8190a
Rendy Artha <rendyartha312@gmail.com>
2021-04-10 00:23:34 +0700

    service product by id

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,55 +1,56 @@
-import { useState } from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-
-import BumbuNasgor from '../assets/images/bumbu-nasigoreng.jpg'
+import { useSelector, useDispatch } from 'react-redux'
+import { getBumbuById } from '../redux/actions/bumbuproduk.actions'
 
 function ProductDetails() {
-  const [productDetail, setProductDetail] = useState({
-    id:0,
-    title: 'Bumbu Nasi Goreng',
-    harga: 'Rp 9.800',
-    img: BumbuNasgor,
-    descriptionText: 'Detail base seasoning, is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,when an unknown printer took a galley of type'
-  })
+  const dispatch = useDispatch()
+  const bumbuProduk = useSelector((state) => state.handleBumbuProduk.dataById)
 
-  const bumbuProduk = useSelector((state) => state.handleBumbuProduk.data)
-  console.log(bumbuProduk);
-  
   let {id} = useParams()
-  console.log(id);
-
-
-
+  useEffect(() => {
+    dispatch(getBumbuById(id))
+  }, [dispatch])
+  
   return (
-    <div className="flex flex-col justify-center items-center h-screen lg:flex-row ">
-        <div className="py-4" key={productDetail.id} >
+    <div className="">
+      {bumbuProduk.map((product) => (
+        <div key={product._id} className="p-4 flex flex-col lg:flex-row lg:justify-between lg:items-center lg:px-10 xl:px-14">
           <img 
-          src={productDetail.img}
-          alt="produkdetail"
-          className="w-full relative lg:w-20"
+            src={product.image} 
+            alt="gambar" 
+            className="rounded w-full lg:max-w-lg"
           />
-        </div>
-        <div className="lg:w-1/3">
-          <div className="font-opensans p-5">
-            <p className="font-opensans font-bold text-2xl pb-2">{productDetail.title}</p>
-            <p className="font-opensans font-bold text-3xl pb-12">{productDetail.harga}</p>
+          <div className="lg:w-1/3">
+          <div className="pt-2">
+            <p className="font-opensans font-bold text-2xl pb-2">{product.name}</p>
+            <p className="font-opensans font-bold text-3xl pb-12">{product.harga}</p>
             <div className="pb-12 lg:w-40">
-              <button className="bg-icon rounded-xl py-2 uppercase text-xs font-opensans font-bold cursor-pointer tracking-wider text-white w-full">ADD CUSTOM</button>
+              <button 
+                className="bg-icon rounded-xl py-2 uppercase text-xs font-opensans font-bold cursor-pointer tracking-wider text-white w-full"
+              >
+                Add Custom
+              </button>
             </div>
             <div className="grid divide-y text-grey">
-
-            <p className="font-opensans uppercase text-sm font-semibold pb-2 text-black">Description</p>
-            <p className="font-opensans text-sm py-5 font-light">{productDetail.descriptionText}</p>
+              <p 
+                className="font-opensans uppercase text-sm font-semibold pb-2 text-black"
+              >
+                Description
+              </p>
+              <p className="font-opensans text-sm py-5 font-light">{product.description}</p>
             </div>
             <div className="pt-5 lg:w-40">
-              <button className="bg-base rounded-xl py-2 text-md font-opensans cursor-pointer tracking-wider text-white filter drop-shadow-base w-full">Add To Cart</button>
+              <button 
+                className="bg-base rounded-xl py-2 text-md font-opensans cursor-pointer tracking-wider text-white filter drop-shadow-base w-full"
+              >
+                Add To Cart
+              </button>
             </div>
           </div>
         </div>
-      <div>
-        
-      </div>
+        </div>
+      ))}
     </div>
   )
 }
