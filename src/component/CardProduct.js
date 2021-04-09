f2666708657588509e20a0e85ff8e26d0d694917
Rendy Artha <rendyartha312@gmail.com>
2021-04-09 22:05:48 +0700

    fixing card

--- src/component/CardProduct.js
+++ src/component/CardProduct.js
@@ -1,33 +1,26 @@
-import React, { useState } from 'react'
-// import BumbuNasgor from '../assets/images/bumbu-nasigoreng.jpg'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 function CardProduct(bumbuProduk) {
-  const isiBumbu = bumbuProduk.bumbuProduk
-
-  // console.log(bumbuProduk);
-  console.log(isiBumbu);
-
+  const productBumbu = bumbuProduk.bumbuProduk
+  console.log(productBumbu)
   return (
-
-    <div className="flex flex-col flex-wrap p-4 md:flex-row md:justify-center lg:justify-between lg:px-5 xl:px-10">
-      { isiBumbu.map((items) => (
-        <Link to={`/productdetails/${items._id}`}>
-          <div className="py-4 px-1 w-72 md:px-4 lg:w-80" key={items._id}>
-            <div className="bg-white rounded overflow-hidden shadow-lg" >
-                <img 
-                  src={items.image}
-                  alt="Logo"
-                  className="w-full h-48 "
-                />
-            
-              <div className="font-opensans p-5">
-                <p className="font-opensans font-bold text-xl pb-2">{items.harga}</p>
-                <p className="font-opensans text-sm">{items.name}</p>
-              </div>
-            </div>
+    <div className="flex flex-col w-full flex-wrap items-center md:flex-row">
+      {productBumbu.map((bumbu) => (
+        <div 
+          className="w-56 h-72 bg-white m-4 rounded shadow-2xl"
+          key={bumbu._id}
+        >
+          <img 
+            src={bumbu.image} 
+            alt="produk"
+            className="w-full h-48 object-cover rounded-t"
+          />
+          <div className="p-2">
+            <h4>{bumbu.name}</h4>
+            <span>Rp. {bumbu.harga}</span>
           </div>
-        </Link> 
+        </div>
       ))}
     </div>
   )
