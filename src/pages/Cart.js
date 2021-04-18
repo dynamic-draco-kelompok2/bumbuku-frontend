dc53e2a9ae9948dd2b038e028af9f1c42ead7ff4
Brianrahmarela <brian.rahmarela@gmail.com>
2021-04-18 14:29:42 +0700

    responsive setting

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getCart } from "../redux/actions/cart.action";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 function Cart() {
 	const dataOrder = useSelector((state) => state.handleCart.data);
@@ -34,7 +34,7 @@ function Cart() {
 				<div>No Order</div>
 			) : (
 				<Container>
-					<Row className="tw-pt-40">
+					<Row className="tw-pt-40 mx-2">
 						<Col>
 							<div>
 								<p className="tw-font-opensans tw-font-bold titleCart tw-m-0">
@@ -45,22 +45,18 @@ function Cart() {
 								</p>
 								<div>
 									<p className="textSubCart tw-lg:w-1/2">
-										Terimakasih User sudah memesan di bumbu Ku! berikut ini
+										Terimakasih sudah memesan di bumbu Ku! berikut ini
 										adalah rincian pesanan anda:
 									</p>
 								</div>
 							</div>
 						</Col>
 					</Row>
-					{/* <div className="tw-bg-base"><p>tes</p></div> */}
 
-					{/* <Container className="tw-bg-black py-5"> */}
-						{dataOrder.map((order, index) => (
-							<>
-							<Row >
-							
-								<Col className="tw-bg-white borderCustom mr-5 mb-5 tw-ml-4">
-								{/* <Col > */}
+					{dataOrder.map((order, index) => (
+						<>
+							<Row>
+								<Col className="tw-bg-white borderCustom mr-5 mb-5 tw-ml-8">
 									<div key={index}>
 										<div className="" key="1">
 											<div className="">
@@ -83,13 +79,11 @@ function Cart() {
 														{order.bumbuProduk_id.description}
 													</p>
 												</div>
-												{/* <div className="tw-bg-black py-5"></div> */}
-
 											</div>
 										</div>
 									</div>
 								</Col>
-								<Col>
+								<Col className="tw-mx-4 tw-mb-2">
 									<h4 className="tw-font-opensans tw-font-bold titleTextCustom">
 										Custom:{" "}
 									</h4>
@@ -112,24 +106,108 @@ function Cart() {
 												</div>
 											</div>
 										))}
-										{/* <div className="tw-bg-black py-5"></div> */}
 								</Col>
-								<div>
-								</div>
+								<div></div>
 							</Row>
-								<div className="lineListOrder">
-									{/* <p >INI LINE PER ITEM</p> */}
-								</div>
-							</>
-						))}
-					{/* </Container> */}
+							<div className="lineListOrder">
+								{/* <p >INI LINE PER ITEM</p> */}
+							</div>
+						</>
+					))}
 
 					<Row>
-						<Col>
-							<p className="tw-font-opensans tw-font-bold titleCart tw-m-0">
-								Kolom Harga
-							</p>
+						<Col className="tw-ml-4 tw-mb-6">
+							<h4 className="text-lg font-semibold font-opensans">
+								Ringkasan Belanja
+							</h4>
 						</Col>
+					</Row>
+
+					<Row>
+						<Col className="tw-ml-4">
+							<Row>
+								<Col>
+									<p className="tw-pyb-2 tw-font-opensans tw-font-regular">
+										Total Harga base:
+									</p>
+								</Col>
+								<Col>
+									<p className="tw-pb-2 tw-font-opensans tw-font-regular">
+										Rp. {totalHargaBase}
+									</p>
+								</Col>
+							</Row>
+							<Row>
+								<Col>
+								<p className="tw-pb-2 tw-font-opensans tw-font-regular">
+								Total Item: 
+							</p>
+								</Col>
+								<Col>
+									<p className="tw-pb-2 tw-font-opensans tw-font-regular">
+									{dataOrder.length} Item
+									</p>
+								</Col>
+							</Row>
+							<Row>
+								<Col>
+								<p className="tw-pb-2 tw-font-opensans tw-font-regular">
+								Total Harga Custom:  
+							</p>
+								</Col>
+								<Col>
+									<p className="tw-pb-2 tw-font-opensans tw-font-regular">
+									Rp. {totalHargaCustom} 
+									</p>
+								</Col>
+							</Row>
+							<Row>
+								<Col>
+								<p className="tw-pb-2 tw-font-opensans tw-font-regular">
+								Total Item Custom:  
+							</p>
+								</Col>
+								<Col>
+									<p className="tw-pb-2 tw-font-opensans tw-font-regular">
+									{CustomItem.length} Item
+									</p>
+								</Col>
+							</Row>
+							<Row>
+								<Col>
+								<p className="tw-font-opensans tw-font-bold txtTotalHarga">
+								Total Harga: 
+							</p>
+								</Col>
+								<Col>
+									<p className="tw-font-opensans tw-font-bold txtTotalHarga">
+									Rp. {totalHargaBase + totalHargaCustom} 
+									</p>
+								</Col>
+							</Row>
+						
+							{/* <p className="tw-pb-2 tw-font-opensans tw-font-regular">
+								Total Harga Custom: Rp. {totalHargaCustom}
+							</p>
+							<p className="tw-pb-2 tw-font-opensans tw-font-regular">
+								Total Item Custom: {CustomItem.length} Item
+							</p>
+							<p className="tw-text-xl tw-font-bold tw-font-regular">
+								Total Harga: Rp. {totalHargaBase + totalHargaCustom}
+							</p> */}
+						</Col>
+					</Row>
+					<div className="linePlaceOrder">
+						{/* <p >INI LINE PER ITEM</p> */}
+					</div>
+					<Row className="tw-pb-40">
+						<Button
+							type="submit"
+							variant="primary"
+							className="mx-4 mt-3 tw-rounded-lg tw-py-2 tw-w-full tw-bg-base tw-border-base tw-shadow-base btnPlaceOrder"
+						>
+							Place Order
+						</Button>
 					</Row>
 				</Container>
 			)}
