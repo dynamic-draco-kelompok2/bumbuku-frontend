ad40d12c64e981ad1e5126ffa7b89dc7d03f1ba5
Brianrahmarela <brian.rahmarela@gmail.com>
2021-04-19 14:15:57 +0700

    adding accordion for custom list order cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getCart } from "../redux/actions/cart.action";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Accordion, Card } from "react-bootstrap";
+import arrowDropdown from "../assets/icons/arrow-dropdown.svg";
 
 function Cart() {
 	const dataOrder = useSelector((state) => state.handleCart.data);
@@ -15,14 +16,19 @@ function Cart() {
 	}, [dispatch]);
 
 	const totalHargaBase = dataOrder.reduce(
-		(total, value) => total + value.bumbuProduk_id.harga, 0
+		(total, value) => total + value.bumbuProduk_id.harga,
+		0
 	);
 
 	return (
 		<>
 			{dataOrder.length === 0 ? (
 				<Container>
-					<Row className="tw-pt-40 mx-2"><p className="tw-font-opensans tw-font-bold titleCart tw-m-0 tw-text-basic">No Order Found</p></Row>
+					<Row className="tw-pt-40 mx-2">
+						<p className="tw-font-opensans tw-font-bold titleCart tw-m-0 tw-text-basic">
+							No Order Found
+						</p>
+					</Row>
 				</Container>
 			) : (
 				<Container className="tw-mt-10">
@@ -34,8 +40,8 @@ function Cart() {
 								</p>
 								<div>
 									<p className="textSubCart tw-lg:w-1/2">
-										Terimakasih sudah memesan di bumbu Ku! berikut ini
-										adalah rincian pesanan anda:
+										Terimakasih sudah memesan di bumbu Ku! berikut ini adalah
+										rincian pesanan anda:
 									</p>
 								</div>
 							</div>
@@ -44,8 +50,13 @@ function Cart() {
 
 					{dataOrder.map((order, index) => (
 						<div key={index}>
-							<Row >
-								<Col className="tw-bg-white borderCustom mr-5 mb-5 tw-ml-8" sm={10} lg={6} md={10}>
+							<Row>
+								<Col
+									className="tw-bg-white borderCustom mr-5 mb-5 tw-ml-8"
+									sm={10}
+									lg={6}
+									md={10}
+								>
 									<div key={index}>
 										<div className="" key="1">
 											<div className="">
@@ -55,10 +66,10 @@ function Cart() {
 													className="tw-rounded tw-w-full tw-object-cover tw-lg:max-w-lg imgBaseCart"
 												/>
 												<h4 className="tw-text-lg tw-font-opensans tw-font-bold textBase">
-													Base : {order.bumbuProduk_id.name}
+													{order.bumbuProduk_id.name}
 												</h4>
 												<p className="tw-pt-1 tw-font-opensans tw-font-regular textHargaBase">
-													Harga : Rp. {order.bumbuProduk_id.harga}
+													Rp. {order.bumbuProduk_id.harga}
 												</p>
 												<div className="tw-grid tw-divide-y tw-text-grey">
 													<p className="tw-font-opensans tw-font-semibold descriptionTitle tw-text-black tw-m-0">
@@ -72,33 +83,56 @@ function Cart() {
 										</div>
 									</div>
 								</Col>
-								{/* {console.log(dataOrder)} */}
-								<Col className="tw-mx-4 tw-mb-2" >
-									<h4 className="tw-font-opensans tw-font-bold titleTextCustom">
-										Custom:{" "}
-									</h4>
-									{!!order.custom &&
-										order.custom.map((custom, index) => (
-											<div
-												key={index}
-												className="tw-flex tw-shadow-2xl borderCustomList "
-											>
-												<img
-													src={custom.bumbuDasar_id.image}
-													alt="gambar custom"
-													className="tw-rounded-lg tw-w-14 tw-h-14 tw-object-cover tw-lg:max-w-lg "
-												/>
-												<div>
-													<p className="tw-y-1 tw-font-opensans tw-font-bold textCustomBumbu">
-														{custom.bumbuDasar_id.name}
-													</p>
-													<p className="textCustomGram">{custom.gram} Gram </p>
-												</div>
-											</div>
-										))}
+
+								<Col className="tw-mx-4 tw-mb-2">
+									{order.custom ? (
+										<Accordion defaultActiveKey="0">
+											<Card>
+												<Accordion.Toggle as={Card.Header} eventKey="0">
+													<Row className="align-items-center ">
+														<Col className="tw-font-opensans tw-font-bold titleTextCustom">Custom</Col>
+														<Col className="">
+															<img
+																src={arrowDropdown}
+																alt="gambar custom"
+																className="tw-w-5 tw-h-5 tw-m-5 tw-pr-0 ml-auto"
+															/>
+														</Col>
+													</Row>
+												</Accordion.Toggle>
+												<Accordion.Collapse eventKey="0">
+													<Card.Body>
+														{!!order.custom &&
+															order.custom.map((custom, index) => (
+																<div
+																	key={index}
+																	className="tw-flex tw-shadow-2xl borderCustomList "
+																>
+																	<img
+																		src={custom.bumbuDasar_id.image}
+																		alt="gambar custom"
+																		className="tw-rounded-lg tw-w-14 tw-h-14 tw-object-cover tw-lg:max-w-lg "
+																	/>
+																	<div>
+																		<p className="tw-y-1 tw-font-opensans tw-font-bold textCustomBumbu">
+																			{custom.bumbuDasar_id.name}
+																		</p>
+																		<p className="textCustomGram">
+																			{custom.gram} Gram{" "}
+																		</p>
+																	</div>
+																</div>
+															))}
+													</Card.Body>
+												</Accordion.Collapse>
+											</Card>
+										</Accordion>
+									) : (
+										<></>
+									)}
 								</Col>
-								<div></div>
 							</Row>
+
 							<Row className="lineListOrder">
 								{/* <p >INI LINE PER ITEM</p> */}
 							</Row>
@@ -129,49 +163,49 @@ function Cart() {
 							</Row>
 							<Row>
 								<Col>
-								<p className="tw-pb-2 tw-font-opensans tw-font-regular">
-								Total Item: 
-							</p>
+									<p className="tw-pb-2 tw-font-opensans tw-font-regular">
+										Total Item:
+									</p>
 								</Col>
 								<Col>
 									<p className="tw-pb-2 tw-font-opensans tw-font-regular">
-									{dataOrder.length} Item
+										{dataOrder.length} Item
 									</p>
 								</Col>
 							</Row>
 							<Row>
 								<Col>
-								<p className="tw-pb-2 tw-font-opensans tw-font-regular">
-								Total Harga Custom:  
-							</p>
+									<p className="tw-pb-2 tw-font-opensans tw-font-regular">
+										Total Harga Custom:
+									</p>
 								</Col>
 								<Col>
 									<p className="tw-pb-2 tw-font-opensans tw-font-regular">
-									Rp. {totalHargaCustom} 
+										Rp. {totalHargaCustom}
 									</p>
 								</Col>
 							</Row>
 							<Row>
 								<Col>
-								<p className="tw-pb-2 tw-font-opensans tw-font-regular">
-								Total Item Custom:  
-							</p>
+									<p className="tw-pb-2 tw-font-opensans tw-font-regular">
+										Total Item Custom:
+									</p>
 								</Col>
 								<Col>
 									<p className="tw-pb-2 tw-font-opensans tw-font-regular">
-									{CustomItem.length} Item
+										{CustomItem.length} Item
 									</p>
 								</Col>
 							</Row>
 							<Row>
 								<Col>
-								<p className="tw-font-opensans tw-font-bold txtTotalHarga">
-								Total Harga: 
-							</p>
+									<p className="tw-font-opensans tw-font-bold txtTotalHarga">
+										Total Harga:
+									</p>
 								</Col>
 								<Col>
 									<p className="tw-font-opensans tw-font-bold txtTotalHarga">
-									Rp. {totalHargaBase + totalHargaCustom} 
+										Rp. {totalHargaBase + totalHargaCustom}
 									</p>
 								</Col>
 							</Row>
