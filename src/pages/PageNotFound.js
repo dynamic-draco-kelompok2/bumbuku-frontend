9d4c402be14f3cd73bfc2eb4974d6738c9f5ad9d
Rendy Artha <rendyartha312@gmail.com>
2021-04-19 13:42:02 +0700

    fix ui page-not-found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -4,19 +4,19 @@ import LogoPageNotFound from '../assets/images/seasoning.png';
 
 const PageNotFound = () => {
   return (
-    <div className="flex flex-col items-center h-screen justify-center">
+    <div className="tw-flex tw-flex-col tw-items-center tw-h-screen tw-justify-center">
       <img 
         src={LogoPageNotFound} 
         alt="pagenotfound" 
-        className="w-40 h-40" 
+        className="tw-w-40 tw-h-40" 
       />
-      <div className="text-center">
-        <h1 className="font-opensans">
+      <div className="tw-text-center">
+        <h1 className="tw-font-opensans">
           404
         </h1>
-        <h2 className="font-opensans">Page Not Found</h2>
-        <div className="bg-base py-2 rounded-full mt-2 text-white">
-          <Link to="/">
+        <h2 className="tw-font-opensans">Page Not Found</h2>
+        <div className="tw-bg-base tw-py-2 tw-rounded-full tw-mt-2">
+          <Link to="/" className="tw-text-white tw-font-opensans">
             Home
           </Link>
         </div>
